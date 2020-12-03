03774326c4de89ff3c5ac73cd34b26936a51f635
Gordon Syme <gordonsyme@gmail.com>
2020-12-03 21:33:33 +0000

    Add runner

---
+++ src/advent_of_code_2020/core.clj
@@ -0,0 +1,11 @@
+(ns advent-of-code-2020.core
+  (:require [clojure.java.io :as io]
+            [clojure.pprint :refer (pprint)]))
+
+(defn -main
+  [input-file day problem]
+  (let [ns-sym (symbol (str "advent-of-code-2020." day))]
+    (require ns-sym)
+    (let [f (ns-resolve ns-sym (symbol problem))]
+      (with-open [r (io/reader input-file)]
+        (pprint (f (line-seq r)))))))
